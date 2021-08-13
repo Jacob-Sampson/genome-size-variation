ddeb68e1cb40d9259b8c0d83d0675093465f8291
Jacob-Sampson <87811901+Jacob-Sampson@users.noreply.github.com>
2021-08-13 08:39:14 +0100

    Update join.sh

--- join.sh
+++ join.sh
@@ -3,7 +3,7 @@ INPUT_FILE_2=""			#Path of the second file to be joined
 OUTPUT_FILE=""			#Path and name of the output file
 
 
-join -o auto -e 0 -a 1 -a 2 INPUT_FILE_1 INPUT_FILE_2 > OUTPUT_FILE
+join -o auto -e 0 -a 1 -a 2 $INPUT_FILE_1 $INPUT_FILE_2 > $OUTPUT_FILE
 
 echo $INPUT_FILE_1 " has been successfully joined to " $INPUT_FILE_2
 
